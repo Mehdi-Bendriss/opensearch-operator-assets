87fccbb9d0ff01dede4b553c91b74119593d42bb
Mehdi-Bendriss <bendrissmehdi@gmail.com>
2022-07-29 11:47:28 +0200

    Added Integration tests execution + serving of tarball via https

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # opensearch-operator-assets
+
+### Run releasing / packaging:
+
+#### Options:
+- `--version`: opensearch version, default: `2.1.0`
+- `--platform`: target platform, default: `linux`
+
+#### Run:
+1. `cd release`
+2. ```
+   bash run.sh
+
+   # or
+    
+   bash run.sh --version 2.1.0 --platform linux
+   ```
--- release/run.sh
+++ release/run.sh
@@ -2,10 +2,48 @@
 
 set -e
 
+# --------------------------------------------------------------------------------
+
+function build_image () {
+    docker build \
+        -t "$3" \
+        --build-arg VERSION="$1" \
+        --build-arg PLATFORM="$2" \
+        --no-cache \
+        --progress=plain .
+}
+
+function run_container () {
+    docker run \
+        -d \
+        -v opensearch:/opensearch \
+        --name "$1" \
+        "$2"
+}
+
+function wait_until_container_exists () {
+    printf "Container '%s': \n" "$1"
+
+    while [ -z "$(docker ps --filter "status=exited" --filter "name=$1" --format '{"ID":"{{ .ID }}"}' | jq .ID)" ]; do
+        printf "\t running... \n"
+        sleep 45s
+    done
+
+    exit_code=$(docker inspect "$1" --format='{{.State.ExitCode}}')
+    if [[ $exit_code -ne 0 ]]; then
+        printf "\t failed. \n"
+        exit 1
+    fi
+
+    printf "\t --- \n\t succeeded... \n"
+}
+
+# --------------------------------------------------------------------------------
+
 pushd docker
 
 # --------------------------------------------------------------------------------
-# Command line args parsing
+# ------------------------- Command line args parsing ----------------------------
 printf "\n ------------- \n Command line parsing... \n ------------- \n"
 
 version=${version:-2.1.0}
@@ -26,56 +64,50 @@ printf "\t Version: %s" "${version}"
 printf "\t Platform: %s" "${platform}"
 
 # --------------------------------------------------------------------------------
-# Packaging
-printf "\n ------------- \n Packaging... \n ------------- \n"
-
-pushd packaging
-
-# Create local volume where to store the tarballs
-docker volume create opensearch
-docker volume inspect opensearch
-
-# Build the Image for packaging a platform specific tarball
-IMAGE_NAME_PACKAGING=opensearch-packaging-"${version}"-"${platform}"
-
-docker build \
-    -t "${IMAGE_NAME_PACKAGING}" \
-    --build-arg VERSION="${version}" \
-    --build-arg PLATFORM="${platform}" \
-    --no-cache \
-    --progress=plain .
-
-# Store the platform-specific tarball in the local volume
-docker run \
-    -d \
-    -v opensearch:/opensearch \
-    "${IMAGE_NAME_PACKAGING}"
-
-popd
+# ------------- Create local volume where to store the tarballs ------------------
+#printf "\n ------------- \n Creating local volumes... \n ------------- \n"
+#
+#docker volume create opensearch
+#docker volume inspect opensearch
+#
+## --------------------------------------------------------------------------------
+## ---------------------------------- Packaging -----------------------------------
+#printf "\n ------------- \n Packaging... \n ------------- \n"
+#
+#pushd packaging
+#
+## Build the Image for packaging a platform specific tarball
+#IMAGE_NAME_PACKAGING=opensearch-packaging-"${version}"-"${platform}"
+## build_image "${version}" "${platform}" "${IMAGE_NAME_PACKAGING}"
+#
+## Store the platform-specific tarball in the local volume
+#run_container "packaging" "${IMAGE_NAME_PACKAGING}"
+#
+#popd
+#
+## --------------------------------------------------------------------------------
+#
+#wait_until_container_exists "packaging"
 
 # --------------------------------------------------------------------------------
-# Testing
+# ---------------------------------- Testing -------------------------------------
 printf "\n ------------- \n Testing... \n ------------- \n"
 
 pushd testing
 
 # Build the Image for running integration tests against the generated platform-specific tarball
 IMAGE_NAME_TESTING=opensearch-testing-"${version}"-"${platform}"
-
-docker build \
-    -t "${IMAGE_NAME_TESTING}" \
-    --build-arg VERSION="${version}" \
-    --build-arg PLATFORM="${platform}" \
-    --no-cache \
-    --progress=plain .
+build_image "${version}" "${platform}" "${IMAGE_NAME_TESTING}"
 
 # Run the integration tests and store a success file upon success
-docker run \
-    -d \
-    -v opensearch:/opensearch \
-    "${IMAGE_NAME_TESTING}"
+run_container "testing" "${IMAGE_NAME_TESTING}"
 
 popd
+
+# --------------------------------------------------------------------------------
+
+wait_until_container_exists "testing"
+
 # --------------------------------------------------------------------------------
 
 popd
